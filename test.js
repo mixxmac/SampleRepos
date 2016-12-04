99f3338b56f30f09a6db3d21f18251650ca64c2e
Yuichi <yuichi@Yuichi-no-iMac.local>
2016-12-05 05:03:38 +0900

    add test

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+const chai = require('chai');
+describe('it', () => {
+    it('add', () => {
+        chai.assert.equal(1, 1);
+    });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInRlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUVBLE1BQVksSUFBSSxXQUFNLE1BQU0sQ0FBQyxDQUFBO0FBRTdCLFFBQVEsQ0FBQyxJQUFJLEVBQUU7SUFDWCxFQUFFLENBQUMsS0FBSyxFQUFFO1FBQ04sSUFBSSxDQUFDLE1BQU0sQ0FBQyxLQUFLLENBQUMsQ0FBQyxFQUFFLENBQUMsQ0FBQyxDQUFDO0lBRTVCLENBQUMsQ0FBQyxDQUFDO0FBQ1AsQ0FBQyxDQUFDLENBQUMifQ==
---
+++ test.ts
@@ -0,0 +1,11 @@
+
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+
+describe('it', () => {
+    it('add', () => {
+        chai.assert.equal(1, 1);
+
+    });
+});
+
