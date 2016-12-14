573bce348c117dcb80e63ca8a351e3b238f56c1c
Yuichi.S <shundo_y@mti.co.jp>
2016-12-14 17:10:01 +0900

    after success追加

--- ts/src/handler.ts
+++ ts/src/handler.ts
@@ -6,7 +6,7 @@ module.exports.hello = (event: any, context: Context, callback: Callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'HELLO ' + event.pathParameters['name'] + '.',
+      message: 'GoodMorning ' + event.pathParameters['name'] + '.',
       stage: 'domain ' + process.env.DOMAIN
     }),
   };
